3e5909a4af85ff3efe88e426df386dc4579da498
luca-masera <lucamasera1995@gmail.com>
2023-11-27 17:16:43 +0100

    primo echo

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+echo 'ciao mi chiamo'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="censura.php" method="GET">
+        <input type="text" name="testo">
+        <input type="submit" value="clicca qui">
+
+    </form>
+</body>
+</html>
